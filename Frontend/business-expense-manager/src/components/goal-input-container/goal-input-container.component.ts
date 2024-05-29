81b675e32eaa53b76dd2a1fb53714356a92a5793
KeeganOReilly <keegan.oreilly@bbd.co.za>
2024-05-29 23:03:41 +0200

    New angular project | Added skeleton components and pages from designs on Figma | Gitignore updates | Readme updates | Default angular app

---
+++ Frontend/business-expense-manager/src/components/goal-input-container/goal-input-container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-goal-input-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './goal-input-container.component.html',
+  styleUrl: './goal-input-container.component.css'
+})
+export class GoalInputContainerComponent {
+
+}
---
+++ Frontend/business-expense-manager/src/components/login-information-input-container/login-information-input-container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-information-input-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-information-input-container.component.html',
+  styleUrl: './login-information-input-container.component.css'
+})
+export class LoginInformationInputContainerComponent {
+
+}
