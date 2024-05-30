a69aa81a78a226f173a22e1a048ebf5839e055b4
cameron <cameron.zwane@bbd.co.za>
2024-05-30 12:54:59 +0200

    feature(login): Added HTML and CSS.
    Note: Some parts will change with the implementation.

--- Frontend/business-expense-manager/src/components/login-information-input-container/login-information-input-container.component.ts
+++ Frontend/business-expense-manager/src/components/login-information-input-container/login-information-input-container.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {LogoComponent} from "../logo/logo.component";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-login-information-input-container',
   standalone: true,
-  imports: [],
+  imports: [
+    LogoComponent,
+    RouterLink,
+  ],
   templateUrl: './login-information-input-container.component.html',
   styleUrl: './login-information-input-container.component.css'
 })
--- Frontend/business-expense-manager/src/pages/login-page/login-page.component.ts
+++ Frontend/business-expense-manager/src/pages/login-page/login-page.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {
+  LoginInformationInputContainerComponent
+} from "../../components/login-information-input-container/login-information-input-container.component";
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [
+    LoginInformationInputContainerComponent
+  ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
