8cce6ec19d32d6778ebeb56a2e1c9c837c08d7a9
KarlEtsebeth <148228664+KarlEtsebeth@users.noreply.github.com>
2024-06-03 20:06:18 +0200

    Add cognito infra

--- Terraform/server-beanstalk-defaults.tf
+++ Terraform/server-beanstalk-defaults.tf
@@ -2,20 +2,6 @@ data "aws_secretsmanager_secret_version" "db_details" {
   secret_id = module.rds.db_instance_master_user_secret_arn
 }
 
-resource "aws_secretsmanager_secret" "auth_secret_key" {
-  name = "auth_secret_key"
-}
-
-resource "aws_secretsmanager_secret_version" "auth_secret_key" {
-  secret_id     = aws_secretsmanager_secret.auth_secret_key.id
-  secret_string = "dummy"
-}
-
-data "aws_secretsmanager_secret_version" "auth_secret_key" {
-  secret_id  = aws_secretsmanager_secret.auth_secret_key.arn
-  depends_on = [aws_secretsmanager_secret_version.auth_secret_key]
-}
-
 resource "aws_s3_object" "backend_beanstalk_default" {
   bucket = aws_s3_bucket.backend_beanstalk.id
   key    = "default_api.zip"
