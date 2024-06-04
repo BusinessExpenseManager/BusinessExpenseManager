c617fb57b5d75d43d8e97a4f91170b4f905b2124
Ryan-Blignaut-BBD <ryan.blignaut@bbd.co.za>
2024-06-04 14:06:05 +0200

    CORS Must be fixed now middleware = trash

--- Backend/Helpers/Cognito/CognitoMiddleware.cs
+++ Backend/Helpers/Cognito/CognitoMiddleware.cs
@@ -1,5 +1,6 @@
 ﻿using System.Security.Claims;
 using Backend.Types;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Backend.Helpers.Cognito;
 
@@ -7,10 +8,21 @@ public class CognitoMiddleware(RequestDelegate next)
 {
     public async Task InvokeAsync(HttpContext context, ICognitoService cognitoService)
     {
-        
-        if (context.User.Identity is { IsAuthenticated: true } &&
-            context.User.FindFirst(ClaimTypes.Email)?.Value != null)
-            cognitoService.Set(new CognitoUser(context.User.FindFirst(ClaimTypes.Email)!.Value));
-        await next(context);
+        if (context.GetEndpoint()?.Metadata.GetMetadata<IAllowAnonymous>() != null)
+        {
+            await next(context);
+            return;
+        }
+
+        var email = context.User.FindFirst(ClaimTypes.Email)?.Value;
+        if (context.User.Identity is { IsAuthenticated: true } && email != null)
+            cognitoService.Set(new CognitoUser(email));
+        else
+        {
+            // Return unauthorized error
+            context.Response.StatusCode = 401; // Unauthorized
+            await context.Response.WriteAsync("Unauthorized: Email not found");
+            return;
+        }
     }
 }
