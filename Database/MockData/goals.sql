cfcae4c140ae032aa48f59fa7591785c8101207a
KeeganOReilly <keegan.oreilly@bbd.co.za>
2024-06-04 15:22:17 +0200

    No touch liquibase

--- Database/MockData/goals.sql
+++ Database/MockData/goals.sql
@@ -1,5 +1,5 @@
 --changeset karl:dml:mockData:goals
 INSERT INTO goals (name, description, monetary_value, due_datetime, created_datetime) VALUES
 ('Dishwaser', 'Save time and will to live', 10000.69, '2024-10-05 10:00:00.0', '2024-06-01 10:00:00.0'),
-('Generator', 'For loadshedding', 20000, '2027-01-01 01:00:00.0', '2024-05-01 10:00:00.0');
+('Stuff', 'Reasons', 200, '2027-01-01 01:00:00.0', '2024-05-01 10:00:00.0');
 --rollback DELETE FROM "goals";
